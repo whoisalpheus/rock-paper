e6e8a19efad30efd3e0d725bf742d80bdd79d059
whoisalpheus <dewitt.jasonm@gmail.com>
2022-09-21 17:45:12 -0400

    Create html, css, js for rps game

---
+++ rockpaper.js
@@ -0,0 +1,8 @@
+// Javascript for Rock Paper Scissors //
+
+// Function that will randomly return "Rock", "Paper", or "Scissors" //
+function getComputerChoice {
+    return "Rock";
+    return "Paper";
+    return "Scissors";
+}
